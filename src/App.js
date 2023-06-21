bf27fa6867032bab9ea566e4fc738ca768edf41b
NBaLogn <36346192+NBaLogn@users.noreply.github.com>
2023-06-21 16:47:43 -0400

    fin

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { CurrentUserLoader } from './CurrentUserLoader';
-import { UserLoader } from './UserLoader';
-import { ResourceLoader } from './ResourceLoader';
-import { ProductInfo } from './ProductInfo';
-import { UserInfo } from './UserInfo';
+import axios from 'axios';
+import {UserInfo} from './UserInfo';
+import {DataSource} from './DataSource';
+
+const getServerData = url => async () => {
+	const response = await axios.get(url);
+	return response.data;
+};
 
 function App() {
 	return (
 		<>
-		<ResourceLoader resourceUrl="/users/123" resourceName="user">
-			<UserInfo />
-		</ResourceLoader>
-		<ResourceLoader resourceUrl="/products/1234" resourceName="product">
-			<ProductInfo />
-		</ResourceLoader>
+			<DataSource getDataFunc={getServerData('users/123')} resourceName={"user"}>
+				<UserInfo />
+			</DataSource>
 		</>
 	);
 }
