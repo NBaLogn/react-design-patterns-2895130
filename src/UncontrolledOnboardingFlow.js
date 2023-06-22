7b99029d6473451bad2ff63d64c6c56217c2b639
NBaLogn <36346192+NBaLogn@users.noreply.github.com>
2023-06-22 10:37:01 -0400

    fin

--- src/UncontrolledOnboardingFlow.js
+++ src/UncontrolledOnboardingFlow.js
@@ -4,9 +4,24 @@ export const UncontrolledOnboardingFlow = ({ children, onFinish }) => {
 	const [onboardingData, setOnboardingData] = useState({});
 	const [currentIndex, setCurrentIndex] = useState(0);
 
-	const goToNext = stepData => {
-		setCurrentIndex(currentIndex + 1);
-	}
+	const goToNext = (stepData) => {
+		const nextIndex = currentIndex + 1;
+
+		const updatedData = {
+			...onboardingData,
+			...stepData,
+		};
+
+		console.log(updatedData);
+
+		if (nextIndex < children.length) {
+			setCurrentIndex(nextIndex);
+		} else {
+			onFinish(updatedData);
+		}
+
+		setOnboardingData(updatedData);
+	};
 
 	const currentChild = React.Children.toArray(children)[currentIndex];
 
@@ -15,4 +30,4 @@ export const UncontrolledOnboardingFlow = ({ children, onFinish }) => {
 	}
 
 	return currentChild;
-}
+};
