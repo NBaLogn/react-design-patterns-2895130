f44c5120895a1c72ec81a75a070eb693981d7df5
NBaLogn <36346192+NBaLogn@users.noreply.github.com>
2023-06-22 09:31:32 -0400

    fin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import { UncontrolledForm } from './UncontrolledForm';
-import { ControlledForm } from './ControlledForm';
-
+import { useState } from 'react';
+import { ControlledModal } from './ControlledModal';
 function App() {
+	const [shouldShowModal, setShouldShowModal] = useState(false);
 	return (
-		<ControlledForm />
+		<>
+			<ControlledModal
+				shouldShow={shouldShowModal}
+				onRequestClose={() => {
+					setShouldShowModal(false);
+				}}>
+				<h1>Hello!</h1>
+			</ControlledModal>
+			<button onClick={() => setShouldShowModal(!shouldShowModal)}>
+				{shouldShowModal ? 'Hide Modal' : 'Show Modal'}
+			</button>
+		</>
 	);
 }
 
