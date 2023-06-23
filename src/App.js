b9acd7a1b0300e0add930e963a55048b86c75794
NBaLogn <36346192+NBaLogn@users.noreply.github.com>
2023-06-23 17:36:21 -0400

    fin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { UserInfo } from './UserInfo';
+import { ProductInfo } from './ProductInfo';
 
 function App() {
 	return (
 		<>
 		<UserInfo userId="123" />
-		<UserInfo userId="234" />
+		<ProductInfo productId="1234" />
 		<UserInfo userId="345" />
 		</>
 	);
